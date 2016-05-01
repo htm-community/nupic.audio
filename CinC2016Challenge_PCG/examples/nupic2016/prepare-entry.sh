cbf2dbfbf66dc945cf4173e9e2f269eaf10c945b
Marek Otahal <markotahal@gmail.com>
2016-05-01 10:46:26 +0200

    start our custom submission in nupic2016/

---
+++ CinC2016Challenge_PCG/examples/nupic2016/prepare-entry.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# file: prepare-entry.sh
+#
+# This script shows how to run the example code (setup.sh and next.sh)
+# over the validation set, in order to produce the list of expected
+# answers (answers.txt) which must be submitted as part of your entry.
+# This script itself does not need to be included in your entry.
+
+set -e
+set -o pipefail
+
+echo "==== running setup script ===="
+
+./setup.sh
+
+echo "==== running entry script on validation set ===="
+
+rm -f answers.txt
+# use GNU parallel if available
+if parallel -k </dev/null >/dev/null 2>/dev/null; then
+    parallel -k --halt=1 < validation/RECORDS \
+        echo {} \; ln -s validation/{}.wav . \; ./next.sh {}
+else
+    for r in `cat validation/RECORDS`; do
+        echo $r
+        ln -sf validation/$r.wav .
+        ./next.sh $r
+    done
+fi
--- CinC2016Challenge_PCG/examples/nupic2016/wav2vect.m
+++ CinC2016Challenge_PCG/examples/nupic2016/wav2vect.m
@@ -1,13 +1,11 @@
 %% Load the trained parameter matrices for Springer's HSMM model.
 % The parameters were trained using 409 heart sounds from MIT heart
 % sound database, i.e., recordings a0001-a0409.
-cd examples/sample2016/
 load('Springer_B_matrix.mat');
 load('Springer_pi_vector.mat');
 load('Springer_total_obs_distribution.mat');
 springer_options   = default_Springer_HSMM_options;
 springer_options.audio_Fs = 1000 %TODO subsample to which value? (higher=longer learning, lower=worse data)
-cd ../..
 
 %% Load data and resample data
 normals = importfile('normals.csv');
@@ -17,13 +15,10 @@ for i=1:1%size(normals)
     r=r(2:end-1)
     [PCG, Fs1] = audioread([r '.wav']);  % load data
     PCG_resampled = resample(PCG,springer_options.audio_Fs,Fs1); % resample to springer_options.audio_Fs (1000 Hz)
-    !pwd
-    cd examples/sample2016/
     % Running runSpringerSegmentationAlgorithm.m to obtain the assigned_states
     [assigned_states] = runSpringerSegmentationAlgorithm(PCG_resampled, springer_options.audio_Fs, Springer_B_matrix, Springer_pi_vector, Springer_total_obs_distribution, false); % obtain the locations for S1, systole, s2 and diastole
     %We dont use features:  Running extractFeaturesFromHsIntervals.m to obtain the features for normal/abnormal heart sound classificaiton
     %features  = extractFeaturesFromHsIntervals(assigned_states,PCG_resampled);
-    cd ../..
 
     reset = zeros(size(PCG_resampled),1);
     reset(1)=1; %construct reset column for HTM
