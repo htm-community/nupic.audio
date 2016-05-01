a5efdbe2fcad688aa138148507748330360eeac5
Marek Otahal <markotahal@gmail.com>
2016-05-01 09:32:54 +0200

    preprocess.sh: select all NORMAL files for training

---
+++ CinC2016Challenge_PCG/preprocess.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+root=$PWD
+dataDirs="data/training-a
+data/training-b
+data/training-c
+data/training-d
+data/training-e"
+destDir="data/normals"
+classType="RECORDS-normal"
+
+mkdir -p $destDir
+for p in $dataDirs; do
+  for r in `cat $p/$classType`; do
+    #ls -l $root/$p/${r}".wav"
+    cp -a $root/$p/${r}".wav" $destDir
+    echo "\"$root/$p/${r}\"," >> normals.csv
+  done
+done
