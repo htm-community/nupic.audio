3284afc42b987f943dac92712520887f999bc599
Marek Otahal <markotahal@gmail.com>
2016-05-06 10:58:05 +0200

    Encoders: fix sound encoders test

--- nupicaudio/encoders/sound_encoders_test.py
+++ nupicaudio/encoders/sound_encoders_test.py
@@ -1,13 +1,13 @@
 # This file will demanstrate use of available sound encoders:
 
 # WAV
-data="./data/a0007.wav"
+data="../examples/data/a0007.wav"
 #from nupicaudio.encoders.wave import WAVEncoder #FIXME move to nupicaudio.examples
-from encoders.wave import WAVEncoder
+from wave import WAVEncoder
 wav2rawEnc = WAVEncoder(data)
 print wav2rawEnc.encode(1)
 
-from nupic.encoders.random_scalar import RandomDistributedScalarEncoder as RDSE
-rdse = RDSE(w=25)
+from nupic.encoders.random_distributed_scalar import RandomDistributedScalarEncoder as RDSE
+rdse = RDSE(resolution=1)
 wav2bitsEnc = WAVEncoder(data, subEncoder=rdse)
 print wav2bitsEnc.encode(1)
