bb1de093c8925961c077f979d83a2a00e7741eed
Marek Otahal <markotahal@gmail.com>
2016-05-12 13:52:48 +0200

    setup: no-namespace pkg

--- setup.py
+++ setup.py
@@ -21,25 +21,28 @@ if __name__ == "__main__":
  setup(
     name="nupic.audio",
     description=description,
-    namespace_packages=["nupic"],
+#    namespace_packages=["nupic"],
     packages=find_packages(),
     install_requires=requirements,
+    package_data = {"nupic/examples/data": ["*.wav"]},
     version="0.1.0",
     author="NuPIC-community",
     author_email="markotahal@gmail.com",
     url="https://github.com/nupic-community/nupic.audio",
-    zip_safe=False
-#    classifiers=[
-#      "Programming Language :: Python",
-#      "Programming Language :: Python :: 2",
-#      "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
-#      "Operating System :: MacOS :: MacOS X",
-#      "Operating System :: POSIX :: Linux",
-#      "Operating System :: Microsoft :: Windows",
-#      # It has to be "5 - Production/Stable" or else pypi rejects it!
-#      "Development Status :: 5 - Production/Stable",
-#      "Environment :: Console",
-#      "Intended Audience :: Science/Research",
-#      "Topic :: Scientific/Engineering :: Artificial Intelligence"
-#    ],
+#    zip_safe=False,
+    classifiers=[
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 2",
+      "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+      "Operating System :: MacOS :: MacOS X",
+      "Operating System :: POSIX :: Linux",
+      "Operating System :: Microsoft :: Windows",
+      # It has to be "5 - Production/Stable" or else pypi rejects it!
+      "Development Status :: 5 - Production/Stable",
+      "Environment :: Console",
+      "Intended Audience :: Science/Research",
+      "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ],
+    license="AGPL",
+    keywords="HTM NuPIC audio AI encoders music",
  )
